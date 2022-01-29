3f55ea3bdcf0122cabfb1ff0795448ef888fea91
Vadim <va7v@ya.ru>
2022-01-29 09:55:25 +0500

    fixes 2

--- app/src/main/java/com/wtsc/spjavatask/MainActivity.java
+++ app/src/main/java/com/wtsc/spjavatask/MainActivity.java
@@ -3,6 +3,8 @@ package com.wtsc.spjavatask;
 import static com.wtsc.spjavatask.Discounter.decryptData;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -13,28 +15,30 @@ import android.widget.Toast;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String MY_SETTINGS = "my_settings";
     private static final String STARTS_COUNTER = "startsCounter";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // для хранения счётчика запусков дефолтный: <имя пакета>_preferences
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences sp = getSharedPreferences(MY_SETTINGS,
+                Context.MODE_PRIVATE);
         int startsCounter = sp.getInt(STARTS_COUNTER, 0);
-        // проверка числа запусков приложения, обновление счётчика
+        // уведомление о 3-м запуске, выравнивание по центру
         if (startsCounter == 3) {
-            // уведомление о 3м запуске, выравнивание по центру
             Toast toast1=Toast.makeText(getApplicationContext(),
                     getString(R.string.msg_3rd_start),
                     Toast.LENGTH_LONG);
             toast1.setGravity(Gravity.CENTER,0,0);
             toast1.show();
         }
+        // обновление счётчика запусков приложения
         SharedPreferences.Editor e = sp.edit();
         startsCounter++;
         e.putInt(STARTS_COUNTER, startsCounter);
         e.apply();
+
         // Задание "Скидка" (консольная версия также в Scratches)
         TextView textView = findViewById(R.id.textView);
         int[] prices = {5,100,20,66,16};
