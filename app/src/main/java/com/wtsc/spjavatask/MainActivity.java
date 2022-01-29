472ecb9fa7ef21f96dab2121c48abf105b3ba0b2
Vadim <va7v@ya.ru>
2022-01-29 09:27:47 +0500

    fixes 2

--- app/src/main/java/com/wtsc/spjavatask/MainActivity.java
+++ app/src/main/java/com/wtsc/spjavatask/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String STARTS_COUNTER = "startsCounter";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +21,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // для хранения счётчика запусков дефолтный: <имя пакета>_preferences
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        int startsCounter = sp.getInt("startsCounter", 0);
+        int startsCounter = sp.getInt(STARTS_COUNTER, 0);
         // проверка числа запусков приложения, обновление счётчика
-        if (startsCounter < 2) {
-            SharedPreferences.Editor e = sp.edit();
-            startsCounter++;
-            e.putInt("startsCounter", startsCounter);
-            e.apply();
-        } else {
-            // уведомление о 3+ запуске, выравнивание по центру
+        if (startsCounter == 3) {
+            // уведомление о 3м запуске, выравнивание по центру
             Toast toast1=Toast.makeText(getApplicationContext(),
                     getString(R.string.msg_3rd_start),
                     Toast.LENGTH_LONG);
             toast1.setGravity(Gravity.CENTER,0,0);
             toast1.show();
         }
+        SharedPreferences.Editor e = sp.edit();
+        startsCounter++;
+        e.putInt(STARTS_COUNTER, startsCounter);
+        e.apply();
         // Задание "Скидка" (консольная версия также в Scratches)
         TextView textView = findViewById(R.id.textView);
         int[] prices = {5,100,20,66,16};
-        textView.setText("Результат: " +
+        textView.setText(getString(R.string.test_result) +
                 Arrays.toString(decryptData(prices, 50, 1, 3)));
     }
 }
