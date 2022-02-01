944100247ff930d39f8646005392ff0438ee2af0
Vadim <va7v@ya.ru>
2022-02-01 21:49:55 +0500

    checking savedInstanceState

--- app/src/main/java/com/wtsc/spjavatask/MainActivity.java
+++ app/src/main/java/com/wtsc/spjavatask/MainActivity.java
@@ -16,28 +16,32 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     private static final String MY_SETTINGS = "my_settings";
-    private static final String STARTS_COUNTER = "startsCounter";
+    private static final String STARTS_COUNTER = "starts_counter";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences sp = getSharedPreferences(MY_SETTINGS,
-                Context.MODE_PRIVATE);
-        int startsCounter = sp.getInt(STARTS_COUNTER, 0);
-        // уведомление о 3-м запуске, выравнивание по центру
-        if (startsCounter == 3) {
-            Toast toast1=Toast.makeText(getApplicationContext(),
-                    getString(R.string.msg_3rd_start),
-                    Toast.LENGTH_LONG);
-            toast1.setGravity(Gravity.CENTER,0,0);
-            toast1.show();
+        // проверка: запущено ли приложение впервые
+        if (savedInstanceState == null)
+        {
+            SharedPreferences sp = getSharedPreferences(MY_SETTINGS,
+                    Context.MODE_PRIVATE);
+            int startsCounter = sp.getInt(STARTS_COUNTER, 0);
+            // уведомление о 3-м запуске, выравнивание по центру
+            if (startsCounter == 3) {
+                Toast toast1=Toast.makeText(getApplicationContext(),
+                        getString(R.string.msg_3rd_start),
+                        Toast.LENGTH_LONG);
+                toast1.setGravity(Gravity.CENTER,0,0);
+                toast1.show();
+            }
+            // обновление счётчика запусков приложения
+            SharedPreferences.Editor e = sp.edit();
+            startsCounter++;
+            e.putInt(STARTS_COUNTER, startsCounter);
+            e.apply();
         }
-        // обновление счётчика запусков приложения
-        SharedPreferences.Editor e = sp.edit();
-        startsCounter++;
-        e.putInt(STARTS_COUNTER, startsCounter);
-        e.apply();
 
         // Задание "Скидка" (консольная версия также в Scratches)
         TextView textView = findViewById(R.id.textView);
